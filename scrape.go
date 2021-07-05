190fae07db0e6cc7d49d8d8b3917b7369353f447
tsamantanis <philip.tsamantanis@icloud.com>
2021-07-05 04:00:41 -0700

    Print all cryptonews headings

--- scrape.go
+++ scrape.go
@@ -12,12 +12,9 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// On every h4 element call callback
+	c.OnHTML("h4", func(e *colly.HTMLElement) {
+		fmt.Println(e.Text)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +22,6 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// Start scraping on https://cryptonews.com/
+	c.Visit("https://cryptonews.com/")
 }
