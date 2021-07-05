0206dbf09db4f3903bf5bbe0115a92a4d80007a5
tsamantanis <philip.tsamantanis@icloud.com>
2021-07-05 04:20:13 -0700

    Save collected data in struct in a serialized json format

--- scrape.go
+++ scrape.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gocolly/colly"
 )
+
+type Data struct {
+	Heading string
+	Link    string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -14,7 +20,21 @@ func main() {
 
 	// On every h4 element call callback
 	c.OnHTML("h4", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+		e.ForEach("a[href]", func(_ int, kf *colly.HTMLElement) {
+			dataTemp := Data{
+				Heading: e.Text,
+				Link:    "https://cryptonews.com" + e.ChildAttr("a", "href"),
+			}
+			js, err := json.MarshalIndent(dataTemp, "", "    ")
+			if err != nil {
+				fmt.Print(err)
+				panic(err)
+			}
+			fmt.Print(string(js))
+			// fmt.Println(js.Link)
+			fmt.Println()
+		})
+
 	})
 
 	// Before making a request print "Visiting ..."
